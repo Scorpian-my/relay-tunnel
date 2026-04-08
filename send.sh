43fa73962c6960fa55e6444cf8a64fa5b371bc0a
Mahyar - Mortezaei <96655803+Scorpian-my@users.noreply.github.com>
2026-04-08 16:09:12 +0330

    Update send.sh

--- send.sh
+++ send.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+mkdir -p outbound
 msg="$1"
 file="outbound/$(date +%s).txt"
+
 echo "$msg" > "$file"
-git add "$file"
-git commit -m "send message"
-git push
+
+git pull --quiet
+git add "$file" --quiet
+git commit -m "send message" --quiet
+git push --quiet
+
+echo "[SENT] $msg"
